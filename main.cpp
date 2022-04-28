a96ea498e1f4fa35682bf088ba8e007ea3a0517f
WassimAbouKhzam <104588610+WassimAbouKhzam@users.noreply.github.com>
2022-04-28 20:36:22 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "animal.h"
+#include "dog.h"
+#include "cat.h"
+#include "fish.h"
+using namespace std;
+
+void printAnimal(Animal* a) {
+	a->print();
+}
+
+int main() {
+	Dog d = Dog(1, 2, 3);
+	Cat c = Cat(4, 5, 6, 7);
+	Fish f = Fish(8, 9, 0, 1);
+
+	Animal* arrayAnimal[3];
+	arrayAnimal[0] = &d;
+	arrayAnimal[1] = &c;
+	arrayAnimal[2] = &f;
+
+	for (int i = 0; i < 3; i++) {
+		printAnimal(arrayAnimal[i]);
+	}
+	d.getOwner(), c.getOwner(), f.getOwner();
+	
+	return 0;
+	
+}
